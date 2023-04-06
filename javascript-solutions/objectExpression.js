445e36b5fda19e3239a913e9a07f77d8afcbc8f1
akim-berezhnoy <berezhnoy.akim@gmail.com>
2023-04-06 03:28:25 +0300

    + JS Hard Distance HW2 + JS Hard Base HW 3

--- javascript-solutions/objectExpression.js
+++ javascript-solutions/objectExpression.js
@@ -7,7 +7,8 @@ function Const(value) {
 Const.prototype = {
     evaluate: function () { return this.value },
     toString: function () { return this.value.toString() },
-    diff:     function () { return new Const(0) },
+    prefix: function() { return this.toString() },
+    diff: function () { return constants.ZERO },
 }
 
 // Variable
@@ -18,144 +19,249 @@ function Variable(name) {
 Variable.prototype = {
     evaluate: function (...args) { return args[argsOrd.indexOf(this.name)] },
     toString: function () { return this.name },
-    diff:     function (differential) { return this.name === differential ? new Const(1) : new Const(0) },
+    prefix: function() { return this.toString() },
+    diff: function (d) { return d === this.name ? constants.ONE : constants.ZERO },
 }
 
-// Expression
-function Expression(...operands) {
+// Operation
+function Operation(...operands) {
     this.operands = operands;
 }
-Expression.prototype = {
+Operation.prototype = {
     evaluate: function (...args) { return this.f(...this.operands.map(operand => operand.evaluate(...args))) },
     toString: function () { return this.operands.reduce((accumulator, operand) => accumulator + operand + " ", '') + this.sign },
-    diff:     function (differential) { return this.diffRule(...this.operands)(differential) },
+    prefix: function() { return '(' + this.sign + this.operands.reduce((accumulator, operand) => accumulator + " " + operand.prefix(), '') + ')'},
+    diff: function (d) { return this.diffRule(...this.operands)(d) },
 }
 
-// Creator of constructors
-function operationConstructorCreator(f, sign, diffRule) {
-    function ExpressionConstructor(...operands) {
-        Expression.call(this, ...operands);
+// Creators
+function createOperation(f, sign, diffRule) {
+    function Constructor(...operands) {
+        Operation.call(this, ...operands);
     }
-    ExpressionConstructor.prototype = {
-        ...Expression.prototype,
-        constructor: ExpressionConstructor,
-        f: f,
-        sign: sign,
-        diffRule: diffRule,
-    }
-    return ExpressionConstructor;
+    Constructor.prototype = Object.create(Operation.prototype);
+    Constructor.prototype.constructor = Constructor;
+    Constructor.prototype.f = f;
+    Constructor.prototype.sign = sign;
+    Constructor.prototype.diffRule = diffRule;
+    return Constructor;
 }
+
+function createConcreteOperation(n, ...params) {
+    let operation = createOperation(...params);
+    operation.prototype.n = n;
+    operation.prototype.sign = operation.prototype.sign + n;
+    return operation;
+}
+
 
 // Operators (binary)
-const Add = operationConstructorCreator(
+const Add = createOperation(
     (a, b) => a + b,
     "+",
     (a, b) => d => new Add(a.diff(d), b.diff(d)),
 )
-const Subtract = operationConstructorCreator(
+const Subtract = createOperation(
     (a, b) => a - b,
     "-",
     (a, b) => d => new Subtract(a.diff(d), b.diff(d)),
 )
-const Multiply = operationConstructorCreator(
+const Multiply = createOperation(
     (a, b) => a * b,
     "*",
-    (a, b) => d => new Add(new Multiply(a.diff(d), b), new Multiply(a, b.diff(d))),
+    (a, b) => d => new Add(
+        new Multiply(a.diff(d), b),
+        new Multiply(a, b.diff(d))
+    ),
 )
-const Divide = operationConstructorCreator(
+const Divide = createOperation(
     (a, b) => a / b,
     "/",
-    (a, b) => d => new Divide(new Subtract(new Multiply(a.diff(d), b), new Multiply(a, b.diff(d))), new Multiply(b, b))
+    (a, b) => d => new Divide(
+        new Subtract(
+            new Multiply(a.diff(d), b),
+            new Multiply(a, b.diff(d))
+        ),
+        new Multiply(b, b)
+    ),
 )
-const Negate = operationConstructorCreator(
+const Negate = createOperation(
     a => -a,
     "negate",
     a => d => new Negate(a.diff(d)),
 )
-const Exp = operationConstructorCreator(
+const Exp = createOperation(
     a => Math.pow(Math.E, a),
     "exp",
-    a => d => new Multiply(new Exp(a), a.diff(d)),
+    a => d => new Multiply(
+        new Exp(a),
+        a.diff(d)
+    ),
 )
-const Ln = operationConstructorCreator(
+const Ln = createOperation(
     a => Math.log(a),
     "ln",
-    a => d => new Multiply(new Divide(new Const(1), a), a.diff(d)),
+    a => d => new Multiply(
+        new Divide(
+            constants.ONE,
+            a
+        ),
+        a.diff(d)
+    )
 )
-const ArcTan = operationConstructorCreator(
-    a => Math.atan(a),
-    "atan",
-    a => d => new Multiply(new Divide(new Const(1), new Add(new Const(1), new Multiply(a, a))), a.diff(d)),
-)
-const ArcTan2 = operationConstructorCreator(
-    (a, b) => Math.atan2(a, b),
-    "atan2",
-    (a, b) => d => new Multiply(new ArcTan(new Divide(a, b)), new Divide(a, b).diff(d)),
-)
-const Sqrt = operationConstructorCreator(
+const Sqrt = createOperation(
     a => Math.sqrt(a),
     "sqrt",
-    a => d => new Multiply(new Divide(new Const(1), (new Multiply(new Const(2), new Sqrt(a)))), a.diff(d)),
+    a => d => new Multiply(
+        new Divide(
+            constants.ONE,
+            new Multiply(
+                constants.TWO,
+                new Sqrt(a)
+            )
+        ),
+        a.diff(d)
+    )
 )
+const ArcTan = createOperation(
+    a => Math.atan(a),
+    "atan",
+    a => d => new Divide(
+        a.diff(d),
+        new Add(
+            constants.ONE,
+            new Multiply(a, a)
+        )
+    ),
+)
+const ArcTan2 = createOperation(
+    (a, b) => Math.atan2(a, b),
+    "atan2",
+    (a, b) => d => new ArcTan(new Divide(a, b)).diff(d),
+)
+const SumSq = createOperation(
+    (...args) => args.map(arg => Math.pow(arg, 2)).reduce((accum, arg) => accum + arg, 0),
+    "sumsq",
+    (...args) => d => args.map(arg => new Multiply(arg, arg).diff(d)).reduce((accum, arg) => new Add(accum, arg), constants.ZERO)
+)
+function SumSqN(n) {
+    return createConcreteOperation(
+        n,
+        SumSq.prototype.f,
+        "sumsq",
+        SumSq.prototype.diffRule
+    )
+}
+function DistanceN(n) {
+    return createConcreteOperation(
+        n,
+        (...args) => Math.sqrt(SumSq.prototype.f(...args)),
+        "distance",
+        (...args) => d => new Sqrt(args.map(arg => new Multiply(arg, arg)).reduce((accum, arg) => new Add(accum, arg), constants.ZERO)).diff(d),
+    )
+}
+
+const Sumsq2 = SumSqN(2);
+const Sumsq3 = SumSqN(3);
+const Sumsq4 = SumSqN(4);
+const Sumsq5 = SumSqN(5);
+const Distance2 = DistanceN(2);
+const Distance3 = DistanceN(3);
+const Distance4 = DistanceN(4);
+const Distance5 = DistanceN(5);
 
 /*
     PARSER
 */
+
+const operators = new Map([
+    ["+", {constructor: Add, args: 2}],
+    ["-", {constructor: Subtract, args: 2}],
+    ["*", {constructor: Multiply, args: 2}],
+    ["/", {constructor: Divide, args: 2}],
+    ["atan2", {constructor: ArcTan2, args: 2}],
+    ["negate", {constructor: Negate, args: 1}],
+    ["exp", {constructor: Exp, args: 1}],
+    ["ln", {constructor: Ln, args: 1}],
+    ["atan", {constructor: ArcTan, args: 1}],
+]);
+
+const multiOperators = new Map([
+    ["sumsq", SumSqN],
+    ["distance", DistanceN]
+]);
+
+const constants = {
+    'ZERO': new Const(0),
+    'ONE': new Const(1),
+    'TWO': new Const(2),
+};
+
+function ParseError(message) {
+    this.message = "ParseError: " + message;
+}
+ParseError.prototype = {
+    ...Object.create(Error.prototype),
+    name: "ParseError",
+    constructor: ParseError,
+};
+const assert = (statement, string) => { if (!statement) throw new ParseError(string); }
+const isOperand = str => !isNaN(str) || argsOrd.includes(str);
+const convertOperand = str => argsOrd.includes(str) ? new Variable(str) : new Const(Number(str));
+
 function parse(str) {
-    const operators = {
-        '+': {
-            f: Add,
-            args: 2
-        },
-        '-': {
-            f: Subtract,
-            args: 2
-        },
-        '*': {
-            f: Multiply,
-            args: 2
-        },
-        '/': {
-            f: Divide,
-            args: 2
-        },
-        'negate': {
-            f: Negate,
-            args: 1
-        },
-        'exp': {
-            f: Exp,
-            args: 1
-        },
-        'ln': {
-            f: Ln,
-            args: 1
-        },
-        'atan': {
-            f: ArcTan,
-            args: 1
-        },
-        'atan2': {
-            f: ArcTan2,
-            args: 2
-        },
+    const convertFunction = (f, n) => new f(...operands.splice(operands.length-n, n));
+    function handleToken(token) {
+        let result;
+        if (isOperand(token)) {
+            result = convertOperand(token);
+        } else if (operators.has(token)) {
+            result = convertFunction(operators.get(token).constructor, operators.get(token).args);
+        } else if (multiOperators.has((token = token.split(/(\d+)/))[0])) {
+            result = convertFunction(multiOperators.get(token[0])(token[1]), token[1]);
+        } else {
+            assert(false, `Unknown type of token: ${token}.`)
+        }
+        return result;
     }
-    const constants = {
-        'one': new Const(1),
-        'two': new Const(2),
-    }
-    let isOperand = str => (!isNaN(str) || argsOrd.includes(str) || str in constants);
-    let convertOperand = str => argsOrd.includes(str) ? new Variable(str) : str in constants ? constants[str] : new Const(Number(str));
-    let convertFunction = (f, n, stack) => {
-        let args = stack.slice(stack.length-n);
-        stack.splice(stack.length-n, n);
-        return new f(...args);
-    }
-    let operands = [], token, tokens = str.split(" ").filter(e => e !== '').reverse();
-    while (tokens.length > 0) {
-        let result = isOperand(token = tokens.pop()) ? convertOperand(token):
-                     convertFunction(operators[token]["f"], operators[token]["args"], operands);
-        operands.push(result);
-    }
+    let operands = [];
+    str.trim().split(/\s+/).forEach(token => operands.push(handleToken(token)));
     return operands.pop();
 }
+
+function parsePrefix(str) {
+    /* Matches any:
+          integer,
+          non-space sequence of characters (в простонародье word)
+          brace (at last brace, that's why braces are matched correctly) */
+    let tokens = str.match(/-?\d+|\w+|\S/g).reverse();
+    function recursiveParse(tokens) {
+        // Take first token
+        let token = tokens.pop();
+        // Check for an operator
+        assert(operators.has(token), `Expected operator, found '${token}'`);
+        let operator = operators.get(token), operands = [];
+        // Take all operands which are needed to apply current operator
+        for (let i = 0; i < operator.args; i++) {
+            token = tokens.pop()
+            // Check for a normal (variable, const or opened bracket) operator start
+            assert(isOperand(token) || token === '(', `Expected operand or opened brace, found '${token}'.`);
+            // Convert and push variable, const or parse and push the whole operand-chunk
+            operands.push(isOperand(token) ? convertOperand(token) : recursiveParse(tokens));
+        }
+        // Make sure that the expression (or single-operator block) lasted with a closed brace
+        assert((token = tokens.pop()) === ')', `Expected closed brace, found '${token}'.`);
+        // Successfully parsed an operator
+        return new operator.constructor(...operands);
+    }
+    // Check for a single-operand expression. If so => leave
+    if (tokens.length === 1 && isOperand(tokens[0])) return convertOperand(tokens.pop());
+    // Check for a normal (opened bracket) start
+    assert(tokens.pop() === '(', `Expected '(' at the start of an expression.`);
+    let result = recursiveParse(tokens);
+    // Check for a normal (nothing left) ending
+    assert(tokens.length === 0, `Expected end of expression, found tokens: ${tokens}.`);
+    // Successfully parsed the whole expression
+    return result;
+}
+
