8318ea534c8274f981e10dfbfc4e3bf4e1be7e50
akim-berezhnoy <berezhnoy.akim@gmail.com>
2023-05-18 16:33:44 +0300

    + 13 HW Prime + Square mod

--- prolog-solutions/primes.pl
+++ prolog-solutions/primes.pl
@@ -3,7 +3,7 @@ prime(N) :- primes(N); ensure_prime(N, 2), assert(primes(N)).
 composite(N) :- \+ prime(N).
 
 prime_divisors(1, []).
-prime_divisors(N, divisors) :- N > 1, remaining_divisors(N, Divisors, 2), !.
+prime_divisors(N, Divisors) :- N > 1, remaining_divisors(N, Divisors, 2), !.
 is_prime_divisor(N, Divisor) :- prime(Divisor), 0 is mod(N, Divisor).
 remaining_divisors(N, [Divisor], _) :- prime(N), Divisor is N.
 remaining_divisors(N, [Divisor | Tail], Candidate) :-
@@ -14,7 +14,7 @@ remaining_divisors(N, [Divisor | Tail], Candidate) :-
     remaining_divisors(N, [Divisor | Tail], Candidate + 1).
 
 square_divisors(1, []).
-square_divisors(N, DoubledPrimes) :- N > 1, prime_divisors(N, Primes), double(Primes, DoubledPrimes).
+square_divisors(N, DoubledPrimes) :- N > 1, prime_divisors(N, Primes), double(m, DoubledPrimes).
 
 double([Head], [Head, Head]).
 double([Head | Tail], [Head, Head | DoubledTail]) :- double(Tail, DoubledTail).
